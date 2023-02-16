6c3760eedea0bbcd1784df864f95df162460abbe
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-02-15 22:32:30 -0700

    meved setUpinput and handleUserinput functions to new file called input.js

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+//connect to the server
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
   });
-
+//translate the message
   conn.setEncoding("utf8");
 
   conn.on("data", (data) => {
@@ -14,19 +15,8 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write ('Name: MF');
-
-    // let delay = 0;
-    // for(let i = 0; i < 100; i++) {
-    //   setTimeout(() => {
-    //   conn.write ('Move: up')}, delay);
-    //   delay +=50
-    // };
-    // setInterval(() => {
-    // conn.write ('Move: up')}, 50);
-    // });
-
     return conn;
   });
 };
 
-module.exports = connect;
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,37 +1,8 @@
-const connect = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 console.log("Connecting ...");
 
 connect();
-
-const handleUserInput = function(key){ 
-    if (key === '\u0003') {
-      process.exit();
-    };
-    if (key === "w") {
-     conn.write('Move: up');
-    }
-    if (key === 'd') {
-    conn.write('Move: right')
-    };
-    if (key === "s") {
-    conn.write('Move: down')
-    };
-    if (key === "a") {
-    conn.write('Move: left')
-    };
-  };
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.on("data", handleUserInput);
-  stdin.resume();
-
-  return stdin;
-  };
-
-
 
 setupInput();
