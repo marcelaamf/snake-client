5c21b151f648e0b09439242b637c39b802380390
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-02-15 22:22:41 -0700

    added stdin and setupInput

--- play.js
+++ play.js
@@ -1,4 +1,37 @@
 const connect = require("./client");
+console.log("Connecting ...");
 
 connect();
-console.log("Connecting ...");
+
+const handleUserInput = function(key){ 
+    if (key === '\u0003') {
+      process.exit();
+    };
+    if (key === "w") {
+     conn.write('Move: up');
+    }
+    if (key === 'd') {
+    conn.write('Move: right')
+    };
+    if (key === "s") {
+    conn.write('Move: down')
+    };
+    if (key === "a") {
+    conn.write('Move: left')
+    };
+  };
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+
+  return stdin;
+  };
+
+
+
+setupInput();
